7a7c4b3b62e0cb12c7f6daaf941a4a53df1cdcc2
Aryan Nipane <nipaneeducation@gmail.com>
2022-02-01 01:31:56 +0530

    end of dtl with for loop

--- 10-django-template-language/courseManagement/course/views.py
+++ 10-django-template-language/courseManagement/course/views.py
@@ -7,5 +7,16 @@ from datetime import datetime
 def viewCourse(request):
     d = datetime.now()
     amount = 233.34434
-    data = {'cname': 'Django', 'dt': d, 'price':amount}
+
+    student = {
+        'name':'Aryan',
+        'age':19,
+        'education':'graduate'
+    }
+
+    names = ['Babu', 'Onkar', 'Aryan', 'Akshay', 'Abhishek']
+
+    car = {'car1':{'name':'ferrari', 'price':200}, 'car2':{'name':'BMW', 'price':100}, 'car3':{'name':'Mercedes', 'price':150}, }
+
+    data = {'cname': 'Django', 'dt': d, 'price':amount, 'vidhyatri':student, 'students':names, 'cars':car}
     return render(request, 'course/django.html', context= data)
